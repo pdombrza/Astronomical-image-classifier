98036f51e9974822d6c7e4ebc535ba8567754da7
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2024-11-24 16:09:19 +0100

    Move network to separate file

--- models.py
+++ models.py
@@ -3,22 +3,52 @@ import torch
 import torch.nn.functional as F
 
 
+class NormActConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+        self.norm_act_conv = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        )
+
+    def forward(self, x):
+        return self.norm_act_conv(x)
+
+
+class NormActLin(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.norm_act_conv = nn.Sequential(
+            nn.BatchNorm1d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.norm_act_conv(x)
+
+
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=0)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv = nn.Sequential(
+            NormActConv(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            NormActConv(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(32*96*96, 1024),
+            NormActLin(1024, 256),
+            NormActLin(256, 32),
+            NormActLin(32, 8)
+        )
+
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        x = self.conv(x)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+        return F.softmax(x, -1)
